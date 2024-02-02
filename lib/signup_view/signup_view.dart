47eb2db1dffd27fad6904d49003d5efc0c45b445
OsamaElewa <osamaelewa424@gmail.com>
2024-02-02 20:50:50 +0200

    make some changes

--- lib/signup_view/signup_view.dart
+++ lib/signup_view/signup_view.dart
@@ -22,7 +22,6 @@ class SignupView extends StatelessWidget {
       create: (context) => RegisterCubit(),
       child: Scaffold(
         backgroundColor: Colors.black,
-        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           leading: IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.arrow_back,color: Colors.white,)),
@@ -33,88 +32,90 @@ class SignupView extends StatelessWidget {
           },
           builder: (context, state) {
             return SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Form(
-                  key: RegisterCubit.get(context).formKey,
-                  child: Column(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text('Let\'s get started',style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
-                          const Text('create an account by filling the information below',style: TextStyle(color: Colors.white,),),
-                          const SizedBox(height: 25,),
-                          TextFieldSection(),
-                          Row(
-                            children: [
-                              Checkbox(
-                                activeColor: Colors.amber,
-                                value: true,
-                                onChanged: (value) {
-                                },
-                              ),
-                              const Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text('By check the box you are agree the terms and',
-                                    style: TextStyle(color: Colors.white,),
-                                  ),
-                                  Text('conditions and privacy policy',
-                                    style: TextStyle(color: Colors.white,),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-
-                          const SizedBox(height: 25,),
-
-                          Container(
-                            width: double.infinity,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              color: Colors.amber,
-                              borderRadius: BorderRadius.circular(15),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Form(
+                    key: RegisterCubit.get(context).formKey,
+                    child: Column(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('Let\'s get started',style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
+                            const Text('create an account by filling the information below',style: TextStyle(color: Colors.white,),),
+                            const SizedBox(height: 25,),
+                            TextFieldSection(),
+                            Row(
+                              children: [
+                                Checkbox(
+                                  activeColor: Colors.amber,
+                                  value: true,
+                                  onChanged: (value) {
+                                  },
+                                ),
+                                const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('By check the box you are agree',
+                                      style: TextStyle(color: Colors.white,),
+                                    ),
+                                    Text('conditions and privacy policy',
+                                      style: TextStyle(color: Colors.white,),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                            child: TextButton(
 
-                              onPressed: () {
-                                if(RegisterCubit.get(context)
-                                    .formKey
-                                    .currentState!
-                                    .validate()){
-                                  CacheHelper.saveData(key: 'regName', value: RegisterCubit.get(context).nameController.text);
-                                  CacheHelper.saveData(key: 'regEmail', value: RegisterCubit.get(context).emailController.text);
-                                  CacheHelper.saveData(key: 'regPassword', value: RegisterCubit.get(context).passwordController.text);
-                                  CacheHelper.saveData(key: 'regPhone', value: RegisterCubit.get(context).phoneController.text);
-                                  print(CacheHelper.getData(key: 'regName'));
-                                  if(CacheHelper.getData(key: 'jobId') == 0){
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => DoctorLayout(),));
-                                  }else if(CacheHelper.getData(key: 'jobId') == 1){
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => EngineerLayout(),));
-                                  } else{
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => AppLayout(),));
+                            const SizedBox(height: 25,),
+
+                            Container(
+                              width: double.infinity,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.amber,
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: TextButton(
+
+                                onPressed: () {
+                                  if(RegisterCubit.get(context)
+                                      .formKey
+                                      .currentState!
+                                      .validate()){
+                                    CacheHelper.saveData(key: 'regName', value: RegisterCubit.get(context).nameController.text);
+                                    CacheHelper.saveData(key: 'regEmail', value: RegisterCubit.get(context).emailController.text);
+                                    CacheHelper.saveData(key: 'regPassword', value: RegisterCubit.get(context).passwordController.text);
+                                    CacheHelper.saveData(key: 'regPhone', value: RegisterCubit.get(context).phoneController.text);
+                                    print(CacheHelper.getData(key: 'regName'));
+                                    if(CacheHelper.getData(key: 'jobId') == 0){
+                                      Navigator.push(context, MaterialPageRoute(builder: (context) => DoctorLayout(),));
+                                    }else if(CacheHelper.getData(key: 'jobId') == 1){
+                                      Navigator.push(context, MaterialPageRoute(builder: (context) => EngineerLayout(),));
+                                    } else{
+                                      Navigator.push(context, MaterialPageRoute(builder: (context) => AppLayout(),));
+                                    }
+                                    showSuccessSnackBar(context: context, message: 'Sign up is done successfully');
                                   }
-                                  showSuccessSnackBar(context: context, message: 'Sign up is done successfully');
-                                }
-                              },
-                              child: const Text('Sign Up',style: TextStyle(color: Colors.black,fontSize: 18),),
+                                },
+                                child: const Text('Sign Up',style: TextStyle(color: Colors.black,fontSize: 18),),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      Spacer(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Already have an account?',style: TextStyle(color: Colors.white),),
-                          TextButton(onPressed: () {
-                            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const SigninView(),));
-                          }, child: Text('Sign in',style: TextStyle(color: Colors.amber),),)
-                        ],
-                      )
-                    ],
+                          ],
+                        ),
+                        //Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Already have an account?',style: TextStyle(color: Colors.white),),
+                            TextButton(onPressed: () {
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const SigninView(),));
+                            }, child: Text('Sign in',style: TextStyle(color: Colors.amber),),)
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
